160b0230ca5367311bded43bf7d535f54b0eef9c
Mariam <mzaim@ece-batch.ecn.purdue.edu>
2015-11-22 16:26:40 -0500

    Bash script for running L1,L2 simulations. Arg1 = debug flags, Arg2=Num of max insts

---
+++ gem5/simulate.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+debugFlags=$1
+insts=$2
+
+# build gem5
+# scons -j 4 ./build/ALPHA_MESI_CMP_directory/gem5.opt
+
+# simulate
+./build/ALPHA_MESI_CMP_directory/gem5.opt --debug-flag=$1 configs/spec2k6/run.py -b sjeng --cpu-type=inorder --caches --maxinsts=$2 --l1i_size=512B --l1d_size=512B --l1d_assoc=2 --l2cache --l2_size=512B --l2_assoc=2
